d7d4eaf06e0caf4eb92a6d5aeb8145e074c4cb42
Louis Maddox <louismmx@gmail.com>
2025-02-02 03:23:50 +0000

    feat: numeric sort

--- src/pols/pols.py
+++ src/pols/pols.py
@@ -2,13 +2,14 @@ from __future__ import annotations
 
 from functools import partial, reduce
 from pathlib import Path
-from sys import stdout
+from sys import argv, stderr, stdout
 from typing import TYPE_CHECKING, Callable, Literal, TypeAlias
 
 import polars as pl
 
 from .features.hide import filter_out_pattern
 from .features.p import append_slash
+from .features.v import numeric_sort
 
 if TYPE_CHECKING:
     import polars as pl
@@ -39,7 +40,7 @@ def pols(
     r: bool = False,
     R: bool = False,
     S: bool = False,
-    sort: Literal["size", "time", "version", "extension"] | None = None,
+    sort: Literal[None, "size", "time", "version", "extension"] = None,
     time: (
         Literal["atime", "access", "use", "ctime", "status", "birth", "creation"] | None
     ) = None,
@@ -54,8 +55,11 @@ def pols(
     # Rest are additions to the ls flags
     keep_path: bool = False,
     keep_fs_metadata: bool = False,
-    print_to: bool = stdout,
+    print_to: TextIO | None = stdout,
+    error_to: TextIO | None = stderr,
     to_dicts: bool = False,
+    raise_on_access: bool = False,
+    debug: bool = False,
 ) -> pl.DataFrame:
     """
     List the contents of a directory as Polars DataFrame.
@@ -64,7 +68,7 @@ def pols(
       [x] a: Do not ignore entries starting with `.`.
       [x] A: Do not list implied `.` and `..`.
       [ ] author: With `l`, print the author of each file.
-      [ ] c: With `l` and `t` sort by, and show, ctime (time of last modification of file
+      [x] c: With `l` and `t` sort by, and show, ctime (time of last modification of file
          status information);
          with `l`: show ctime and  sort  by name;
          otherwise: sort by ctime, newest first.
@@ -89,7 +93,7 @@ def pols(
       [ ] r: Reverse order while sorting.
       [ ] R: List directories recursively.
       [ ] S: Sort by file size, largest first.
-      [ ] sort: sort by WORD instead of name: None (`U`), size (`S`), time (`t`), version
+      [x] sort: sort by WORD instead of name: none (`U`), size (`S`), time (`t`), version
             (`v`), extension (`X`).
       [ ] time: change  the default of using modification times:
               - access time (`u`): atime, access, use
@@ -104,12 +108,14 @@ def pols(
       [ ] U: Do not sort; list entries in directory order.
       [ ] v: Natural sort of (version) numbers within text, i.e. numeric, non-lexicographic
          (so "file2" comes after "file10" etc.).
-      [ ] X: Sort alphabetically by entry extension.
-      [ ] t: Sort by time, newest first
+      [x] X: Sort alphabetically by entry extension.
+      [x] t: Sort by time, newest first
       [x] keep_path: Keep a path column with the Pathlib path object.
       [x] keep_fs_metadata: Keep filesystem metadata booleans: `is_dir`, `is_symlink`.
-      [x] print_to: Where to print to, by default prints to STDOUT, `False` to turn off.
+      [x] print_to: Where to print to, by default writes to STDOUT, `None` to disable.
+      [x] error_to: Where to error to, by default writes to STDERR, `None` to disable.
       [x] to_dicts: Return the result as dicts.
+      [x] raise_on_access: Raise an error if a file cannot be accessed.
 
         >>> pls()
         shape: (77, 2)
@@ -154,27 +160,94 @@ def pols(
             expanded_paths.append(path)
 
     for path in expanded_paths:
-        if path.is_file():
+        try:
+            is_file = path.is_file()
+        except OSError as e:
+            print(f"pols: cannot access '{path}': {e}", file=error_to)
+            if raise_on_access:
+                raise
+            continue
+        if is_file:
             individual_files.append(path)
         elif path.is_dir():
             dirs_to_scan.append(path)
         elif not path.exists():
             nonexistent.append(
                 FileNotFoundError(
-                    f"pols: cannot access 'path': No such file or directory"
+                    f"pols: cannot access '{path}': No such file or directory"
                 )
             )
     if nonexistent:
-        raise FileNotFoundError(f"No such file:")
+        excs = ExceptionGroup(f"No such file:", nonexistent)
+        if raise_on_access:
+            raise excs
+        else:
+            print(excs, file=error_to)
+
+    sort_pipes = []
+    # none (`U`), size (`S`), time (`t`), version (`v`), extension (`X`).
+
+    # Recreate the CLI order, N.B. will not be ordered from Python library call
+    # (unsure if there's a workaround using inspect?)
+    sortable = {"sort", "S", "t", "v", "c", "X"}
+    # Take the flags and use their local values (i.e. parsed param values)
+    sort_order = [k.lstrip("-") for k in argv if k.lstrip("-") in sortable]
+    sort_sequence = {sort_key: locals()[sort_key] for sort_key in sort_order}
+    sort_lookup = {
+        "none": "U",
+        "size": "S",
+        "time": "t",
+        "version": "v",
+        "extension": "X",
+    }
+    # If a `--sort` was specified, set the corresponding value to True
+    if "sort" in sort_sequence:
+        sort_ref = sort_lookup[sort_sequence["sort"]]
+        # Cannot simply set it to True as it would be last in the order
+        ss_idx = (ss_lst := list(sort_sequence.items())).index("sort")
+        # Overwrites the sort flag with the referenced flag with a value of True
+        sort_sequence = dict([*ss_lst[:ss_idx, (sort_ref, True), ss_lst[ss_idx + 1 :]]])
+
+    if sort_sequence:
+        # Sort in reverse order of specification so sorts given first are applied last
+        for sort_flag, sort_val in reversed(sort_sequence.items()):
+            if sort_val is False:
+                continue
+            match sort_flag:
+                case "U":
+                    continue  # Do not sort
+                case "S":
+                    # This may cause a `MapWithoutReturnDtypeWarning` but it errors with
+                    # `return_dtype` set as either int or pl.Int64 but works without!
+                    # TODO: change this to a function
+                    sort_by = pl.col("path").map_elements(lambda p: p.stat().st_size)
+                # case "t":
+                #     sort_by = pl.col("name").str.split(".").list.last()
+                case "v":
+                    sort_by = numeric_sort(pl.col("name"))
+                case "X":
+                    sort_by = pl.col("name").str.split(".").list.last()
+                case _:
+                    raise ValueError(f"Invalid flag in sort sequence {sort_flag}")
+
+            sort_func = lambda df: df.sort(by=sort_by, maintain_order=True)
+            sort_pipes.append(sort_func)
+    else:
+        lexico_sort = lambda df: df.sort(
+            by=pl.col("name").str.to_lowercase(), maintain_order=True
+        )
+        sort_pipes.append(lexico_sort)
 
     pipes = [
         *([partial(filter_out_pattern, pattern=hide)] if hide else []),
         # Add symlink and directory bools from Path methods
         add_path_metadata,
         *([append_slash] if p else []),
+        *([] if U else sort_pipes),
     ]
 
     results = []
+    failures = []
     for idx, path_set in enumerate((individual_files, *dirs_to_scan)):
         is_dir = idx > 0
         if not path_set:
@@ -209,8 +282,12 @@ def pols(
                 file_entries.append(entry)
             files = pl.DataFrame(file_entries)
         except Exception as e:
-            print(f"Got no files from {path_set} due to {e}")
-            raise
+            failures.extend([ValueError(f"Got no files from {path_set} due to {e}"), e])
+            if raise_on_access:
+                raise e
+            else:
+                print(e, file=error_to)
+            continue
         path_set_result = reduce(pl.DataFrame.pipe, pipes, files).drop(drop_cols)
         # print(path_set_result.to_dicts())
         results.append(
@@ -222,6 +299,8 @@ def pols(
             if source:
                 print(f"{source}:", file=print_to)
             print(paths, file=print_to)
+            if debug:
+                breakpoint()
     return results if to_dicts else None
 
 
